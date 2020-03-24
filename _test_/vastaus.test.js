887dc10a088143a9f1d3c84eeb271eba9b8d8b60
Niina Siitari <siitari.niina@gmail.com>
2020-03-24 11:12:45 +0200

    Tehdyt testit arvattaville sanoille toimii. Muutettu poolin yhteystietoja laittamalla tunnus ja salasana enviin.

--- _test_/vastaus.test.js
+++ _test_/vastaus.test.js
@@ -1,12 +1,22 @@
-const supertest = require('supertest');
 const app = require('../app');
-const baseurl = '/api/piirtoalias';
+const supertest = require('supertest');
+const request = supertest(app)
+const baseurl = '/words';
 
-test('Saamme haettua arvattavan sanan tietokannasta', (done) => {
-    supertest(app)
-        .get(baseurl+'/arvattavasana')
-        .then(response => {
-            expect(response.statusCode).toBe(200);
-            done();
-        })
+describe('Sanojen haku onnistuu kannasta', () => {
+
+    it('saa haettua kaikki sanat tietokannasta', async (done) => {
+        const response = await request.get(baseurl + '/')
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBe(35); //testidatan koko on 35
+        done();
+    });
+
+    it('saa haettua yhden sanan tietokannasta', async (done) => {
+        const response = await request.get(baseurl + '/2') //hakee sanan jonka id on 2
+        expect(response.statusCode).toBe(200);
+        expect(response.body.word).toContain('ampua');
+        done();
+    });
 });
+
